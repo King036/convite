53075a727a72d68a8cf85d4173d5ab1a4e1f7191
Ryan Lima <107500867+King036@users.noreply.github.com>
2026-04-20 22:05:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask,render_template
+import psycopg2
+from flask import Flask,render_template ,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('convite.html')
+
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ.get("dpg-d7jcgk57vvec738s275g-a"),
+        database=os.environ.get("convite_skn0"),
+        user=os.environ.get("convite_skn0_user"),
+        password=os.environ.get("DubGmVGAiMP8QYkHcTGODKVQxlc65UCw"),
+        port=os.environ.get("DB_PORT", 5432)
+    )
+    return conn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
